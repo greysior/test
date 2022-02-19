6fc3721b6c836f87c6b1f212eba2c21051be7e92
greysior <gramadhan877@gmail.com>
2022-02-19 17:14:44 +0700

    perubahan1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajarweb</title>
+</head>
+<body>
+    <h1>belajar</h1>
+    <p>belajar</p>
+    <br>
+    <h6>belajar</h6>
+    <h2>belaharrr</h2>
+    
+</body>
+</html>
